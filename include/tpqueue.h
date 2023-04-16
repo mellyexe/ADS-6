8cbfae55eef653d3297ddb0da9746117fbd5eebe
mellyexe <125124784+mellyexe@users.noreply.github.com>
2023-04-16 17:36:53 +0300

    Update tpqueue.h

--- include/tpqueue.h
+++ include/tpqueue.h
@@ -4,7 +4,45 @@
 
 template<typename T, int size>
 class TPQueue {
-  // реализация шаблона очереди с приоритетом на кольцевом буфере
+private:
+    T* arr;
+    int first, last, count;
+
+public:
+    TPQueue() : first(0), last(0), count(0) {
+        arr = new T[size];
+    }
+    ~TPQueue() {
+        delete[] arr;
+    }
+    bool isEmpty() const {
+        return 0 == count;
+    }
+    bool isFull() const {
+        return count == size;
+    }
+    void push(const T& value) {
+        if (isFull()) {
+            throw std::string("Full!");
+        } else {
+            int index = first;
+            while (index != last && arr[index].prior >= value.prior)
+                index = ++index % size;
+            for (int i = last; i != index; i = (--i + size) % size)
+                arr[i] = arr[((i - 1) + size) % size];
+            arr[index] = value;
+            last = ++last % size;
+            ++count;
+        }
+    }
+    const T& pop() {
+        if (isEmpty()) {
+            throw std::string("Empty!");
+        } else {
+            --count;
+            return arr[first++ % size];
+        }
+    }
 };
 
 struct SYM {
